140cf4218f6b5cfb5b4899dd06e837fe87aa48c5
AbdulRahman ElRawas <alrawas.abed@gmail.com>
2024-04-24 13:11:35 +0300

    context, context propagation, spying, extending http test by writing a spyresponsewriter because newrecorder doesn't record the errors?

---
+++ context/context.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+)
+
+type Store interface {
+	Fetch(ctx context.Context) (string, error)
+	// Cancel()
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		data, err := store.Fetch(r.Context())
+
+		if err != nil {
+			return // todo: log error however you like
+		}
+
+		fmt.Fprint(w, data)
+	}
+}
+
+// We can see after this that the server code has become simplified
+// as it's no longer explicitly responsible for cancellation,
+// it simply passes through context and relies on the downstream functions
+// to respect any cancellations that may occur.
