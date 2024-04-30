89c26e84330fc55395c6d79bd4f811088a7fd3c3
AbdulRahman ElRawas <alrawas.abed@gmail.com>
2024-04-30 22:11:12 +0300

    start templating exercise

---
+++ blogrenderer/blogrenderer.go
@@ -0,0 +1,17 @@
+package blogrenderer
+
+import (
+	"fmt"
+	"io"
+)
+
+// if you're continuing from the read files chapter, you shouldn't redefine this
+type Post struct {
+	Title, Description, Body string
+	Tags                     []string
+}
+
+func Render(w io.Writer, p Post) error {
+	_, err := fmt.Fprintf(w, "<h1>%s</h1>", p.Title)
+	return err
+}
